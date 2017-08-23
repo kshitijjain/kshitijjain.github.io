0120ad2ec8bb6083c55d28c205005dc90539c326
Kshitij Jain (Digital) <KS324287@wipro.com>
2017-08-24 01:28:41 +0530

    portfolio added

---
+++ javascripts/script.js
@@ -0,0 +1,23 @@
+angular.module("myPortfolio", ["ngMaterial"])
+.config(function($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+    .primaryPalette('red', {
+      'default': '900', 
+      'hue-1': '100', 
+      'hue-2': '600',
+      'hue-3': 'A100'
+    })
+    .accentPalette('indigo');
+})
+.factory("metaDataProvider", ["$http", function($http){
+    var metaData= {};
+    metaData.getMetaData= function(){
+        return $http.post("data/meta-data.json");
+    };
+    return metaData;
+}])
+.controller("portfolioController", ["$scope", "metaDataProvider", function($scope, metaDataProvider){
+    metaDataProvider.getMetaData().then(function(data){
+        $scope.metaData= data.data;
+    });
+}]);
